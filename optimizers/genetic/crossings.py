1ddd4ab11566ccfd1b2d669c2779ff6e1a054e1b
Lukasz Klimkiewicz <ukasz.klimkiewicz@gmail.com>
2022-10-27 22:30:11 +0200

    Add crossing

--- optimizers/genetic/crossings.py
+++ optimizers/genetic/crossings.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from abc import ABC, abstractmethod
 
 
@@ -11,7 +13,19 @@ class BaseCrossing(ABC):
 class MultiPointCrossing(BaseCrossing):
 
     def __init__(self, n_points):
-        pass
+        self.n_points = n_points
 
     def __call__(self, first_parent, second_parent):
-        pass
+        split_indexes = np.arange(1, len(first_parent)+1)
+        np.random.shuffle(split_indexes)
+        split_indexes = split_indexes[:self.n_points]
+
+        merged_parents = np.row_stack([first_parent, second_parent])
+        parts = np.split(merged_parents, split_indexes, axis=1)
+
+        for part in parts:
+            np.random.shuffle(part)
+
+        reordered_merged = np.hstack(parts)
+        first_child, second_child = reordered_merged[0], reordered_merged[1]
+        return first_child, second_child
