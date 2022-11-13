47a8c897a362921620b3cc403ac56a0c8e93113f
Lukasz Klimkiewicz <ukasz.klimkiewicz@gmail.com>
2022-11-13 12:30:58 +0100

    Share problem

---
+++ optimizers/swarm/optimizer.py
@@ -0,0 +1,100 @@
+import numpy as np
+
+from .. import BaseOptimizer
+from .solver import PSOSolver
+
+
+class SwarmOptimizer(BaseOptimizer):
+
+    def __init__(
+            self,
+
+            # swarm optimization params
+            n_particles=100,
+            w=0.95,
+            c1=0.25,
+            c2=0.25,
+            v=0.5,
+
+            # stop conditions
+            max_iters=1000,
+            tol=1e-3,
+            n_tol=0.7,
+
+            # what should learn
+            learn_premises=True,
+            learn_operators=True,
+            learn_consequents=True,
+
+            # parameters bounds
+            bounds_premises=(0, 4),
+            bounds_operators=(0, 2),
+            bounds_consequents=(0, 2),
+    ):
+        self.solver = PSOSolver(n_particles, w, c1, c2, v, max_iters, tol, n_tol)
+
+        self.bounds_premises = bounds_premises
+        self.bounds_operators = bounds_operators
+        self.bounds_consequents = bounds_consequents
+
+        self.learn_premises = learn_premises
+        self.learn_operators = learn_operators
+        self.learn_consequents = learn_consequents
+
+        # aux variables
+        self.anfis = None
+        self.premises_length = 0
+        self.operators_length = 0
+        self.consequents_length = 0
+
+    def optimize(self, anfis):
+        self._init_aux_variables(anfis)
+        constraints = self._construct_domain_constraints()
+        best_particle, _ = self.solver.solve(self._optimized_function, constraints)
+
+    def _init_aux_variables(self, anfis):
+        self.anfix = anfis
+        self.premises_length = np.array(anfis.premises).size
+        self.operators_length = len(anfis.op)
+        self.consequents_length = len(anfis.tsk.flatten())
+
+    def _construct_domain_constraints(self):
+        premises_constraints = [self.bounds_premises] * self.premises_length
+        operators_constraints = [self.bounds_operators] * self.operators_length
+        consequents_constraints = [self.bounds_consequents] * self.consequents_length
+        return [*premises_constraints, *operators_constraints, *consequents_constraints]
+
+    def _optimized_function(self, particle):
+        weights = self._get_weights_from_particle(particle)
+        self._config_anfis_from_weights(self.anfis, weights)
+        predicted_labels = self.anfis.estimate_labels()
+        error = np.sum(np.abs(predicted_labels - self.anfis.expected_labels))
+        fitness = 1 / error
+        return fitness
+
+    def _get_weights_from_particle(self, particle):
+        all_weights = []
+        current_pos = 0
+
+        learn_flags = [self.learn_premises, self.learn_operators, self.learn_consequents]
+        params_lengths = [self.premises_length, self.operators_length, self.consequents_length]
+
+        for learn_flag, param_length in zip(learn_flags, params_lengths):
+            if learn_flag:
+                weights = particle[current_pos:current_pos + param_length]
+                all_weights.append(weights)
+                current_pos += param_length
+            else:
+                all_weights.append(None)
+
+        return all_weights
+
+    @staticmethod
+    def _config_anfis_from_weights(anfis, weights):
+        premises, operators, consequents = weights
+        if premises is not None:
+            anfis.set_premises_parameters(premises)
+        if operators is not None:
+            anfis.set_op_parameter(operators)
+        if consequents is not None:
+            anfis.set_tsk_parameter(consequents)
---
+++ optimizers/swarm/solver.py
@@ -0,0 +1,124 @@
+import numpy as np
+
+
+class PSOSolver:
+
+    def __init__(self, n_particles=100, w=0.95, c1=0.25, c2=0.25, v=0.5, max_iters=1000, tol=1e-3, n_tol=0.7):
+        self.n_particles = n_particles
+        self.w = np.broadcast_to(w, (n_particles,))
+        self.v = np.broadcast_to(v, (n_particles,))
+        self.c1 = np.broadcast_to(c1, (n_particles,))
+        self.c2 = np.broadcast_to(c2, (n_particles,))
+        self.max_iters = int(max_iters)
+        self.tol = tol
+        self.n_tol = n_tol if isinstance(n_tol, int) else n_tol * n_particles
+
+    def solve(self, fun, domain, *, callback=None):
+        domain = np.array(domain)
+        n_dim = domain.shape[0]
+
+        # state variables
+        pos = self._random_positions(domain)                    # particle positions
+        vel = self._random_velocities(n_dim)                    # particle velocities
+        inv = np.zeros((self.n_particles,), dtype=np.bool_)     # invalid particles (outside domain)
+        lv = np.repeat(np.inf, self.n_particles)                # local minimum values
+        lp = np.zeros((self.n_particles, n_dim))                # local minimum positions
+        gv = np.inf                                             # global minimum value
+        gp = np.zeros((n_dim,))                                 # global minimum position
+
+        for i in range(self.max_iters):
+
+            # updating state
+            lv, lp, gv, gp = self._get_minimums(lv, lp, gv, gp, fun, pos, inv)
+            vel = self._get_new_velocities(pos, vel, lp, gp, inv)
+            pos = self._get_new_positions(pos, vel)
+            vel, inv = self._bounce_particles_outside_domain(pos, vel, domain)
+
+            # early stop
+            if self._early_stop(pos, gp):
+                break
+
+            # optional callback
+            if callback:
+                callback(gp, gv, pos)
+
+        return gp, gv
+
+    def _random_positions(self, domain):
+        dims_pos = []
+        for constraint in domain:
+            start, end = constraint
+            dim_pos = np.random.rand(self.n_particles) * (end - start) + start
+            dims_pos.append(dim_pos)
+        pos = np.column_stack(dims_pos)
+        return pos
+
+    def _random_velocities(self, n_dims):
+        directions = np.random.rand(self.n_particles, n_dims)
+        lengths = np.sqrt(directions[:, 0]**2 + directions[:, 1]**2)
+        scale_factors = self.v / lengths
+        scale_factors_transformed = np.repeat(np.expand_dims(scale_factors, axis=1), 2, axis=1)
+        scaled_directions = directions * scale_factors_transformed
+        return scaled_directions
+
+    def _get_minimums(self, lv, lp, gv, gp, fun, pos, inv):
+        # calling function
+        values = self._get_function_values(fun, pos, inv)
+
+        # local minimums
+        where_better = values < lv
+        lv[where_better] = values[where_better]
+        lp[where_better] = pos[where_better]
+
+        # global minimum
+        min_index = np.argmin(values)
+        min_value = values[min_index]
+        if min_value < gv:
+            gv = min_value
+            gp = pos[min_index]
+
+        return lv, lp, gv, gp
+
+    @staticmethod
+    def _get_function_values(fun, pos, invalid_pos):
+        return np.array([fun(args) if not invalid else np.inf for args, invalid in zip(pos, invalid_pos)])
+
+    def _get_new_velocities(self, pos, vel, lp, gp, inv):
+        r1, r2 = np.random.rand(2)
+        g_diff = gp - pos
+        l_diff = lp - pos
+        main_term = vel * self.w[..., None]
+        local_term = l_diff * r1 * self.c1[..., None]
+        global_term = g_diff * r2 * self.c2[..., None]
+        new_vel = main_term + local_term + global_term
+        mixed_vel = np.where(np.repeat(inv[..., None], vel.shape[1], axis=1), vel, new_vel)
+        return mixed_vel
+
+    @staticmethod
+    def _get_new_positions(pos, vel):
+        return pos + vel
+
+    def _bounce_particles_outside_domain(self, pos, vel, domain):
+        invalid = np.zeros((self.n_particles,), dtype=np.bool_)
+        new_vel = vel.copy()
+
+        for dim, bounds in enumerate(domain):
+            lower_bound, upper_bound = bounds
+
+            # mark particles as invalid
+            dim_invalid = (pos[:, dim] < lower_bound) | (pos[:, dim] > upper_bound)
+            invalid |= dim_invalid
+
+            # flip particles velocities
+            new_vel[dim_invalid, dim] *= -1
+
+        return new_vel, invalid
+
+    def _early_stop(self, pos, gp):
+        if self.tol is None:
+            return
+        diffs = pos - gp
+        distances = np.sqrt(diffs[:, 0] ** 2 + diffs[:, 1] ** 2)
+        acceptable = distances < self.tol
+        acceptable_count = np.sum(acceptable)
+        return acceptable_count >= self.n_tol
