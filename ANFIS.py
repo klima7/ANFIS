13b1b823634db342d5a5679da6d5031685d530d8
Lukasz Klimkiewicz <ukasz.klimkiewicz@gmail.com>
2022-11-24 22:39:31 +0100

    Add t-norms and s-norms support

--- ANFIS.py
+++ ANFIS.py
@@ -3,35 +3,40 @@ from time import time
 
 import numpy as np
 import matplotlib.pyplot as plt
+from itertools import product
 
 from helps_and_enhancers import calculate_combinations
-from operators import productN
 from optimizers import BaseOptimizer
 
 
 class ANFIS:
 
-    def __init__(self, inputs, training_data: np.ndarray, expected_labels: np.ndarray, operator_function=productN,
+    def __init__(self, inputs, training_data: np.ndarray, expected_labels: np.ndarray, t_norm, s_norm,
                  operator_init_value=0.5):
         self.input_list = inputs
         self.input_number = len(inputs)
         self.training_data = training_data
         self.expected_labels = expected_labels
+        self.t_norm = t_norm
+        self.s_norm = s_norm
 
         self.premises = []
         for i in range(self.input_number):
             self.premises.append(self.input_list[i].get())
 
-        self.nodes_number = np.prod([inp.n_functions for inp in self.input_list])
+        self.premises_combinations = np.array(calculate_combinations(self))[:, ::-1]
+        self.operators_variations = list(product([self.t_norm, self.s_norm], repeat=3))
 
-        self.operator_function = operator_function
+        mf_combinations_count = np.prod([inp.n_functions for inp in self.input_list])
+        op_variations_count = len(self.operators_variations)
+        nodes_number = mf_combinations_count * op_variations_count
 
-        # self.tsk = np.ones((self.nodes_number ,self.input_number+1))
-        self.tsk = np.random.random((self.nodes_number, self.input_number + 1))
+        print(f'Combinations of membership functions: {mf_combinations_count}')
+        print(f'Variations of operators: {op_variations_count}')
+        print(f'Total rules count: {nodes_number}')
 
-        self.op = [operator_init_value] * self.nodes_number
-
-        self.calculate_aids()
+        self.tsk = np.random.random((nodes_number, self.input_number + 1))
+        self.op = [operator_init_value] * nodes_number
 
     # ----------------------- aux -----------------------
 
@@ -47,9 +52,6 @@ class ANFIS:
     def set_op_parameter(self, op):
         self.op = np.array(op).flatten()
 
-    def calculate_aids(self):
-        self.premises_combinations = np.array(calculate_combinations(self))[:, ::-1]
-
     # -------------------- estimating --------------------
 
     def output_to_labels(self, y_pred):
@@ -62,7 +64,6 @@ class ANFIS:
 
     def anfis_estimate_labels(self, fv, op, tsk) -> np.ndarray:
         data = self.training_data
-
         self.set_premises_parameters(fv)
         tsk = np.reshape(tsk, np.shape(self.tsk))
         memberships = [self.input_list[x].fuzzify(data[x]) for x in range(self.input_number)]
@@ -76,8 +77,7 @@ class ANFIS:
             arguments.append(item)
 
         arguments = np.transpose(arguments, (1, 2, 0))
-
-        R = self.operator_function(arguments, op)
+        R = self.apply_all_operators_variations(arguments)
 
         # Normalizacja normalizacja poziomów aktywacji reguł
         Rsum = np.sum(R, axis=1, keepdims=True)
@@ -93,6 +93,20 @@ class ANFIS:
         result = (Q * Rnorm).sum(axis=1, keepdims=True)
 
         return result.T
+
+    def apply_all_operators_variations(self, arguments):
+        partial_results = []
+        for operators_variation in self.operators_variations:
+            partial_result = self.apply_single_operators_variation(arguments, operators_variation)
+            partial_results.append(partial_result)
+        return np.hstack(partial_results)
+
+    @staticmethod
+    def apply_single_operators_variation(arguments, operators_variation):
+        result = operators_variation[0](np.array([arguments[0], arguments[1]]), None)
+        for i in range(1, len(operators_variation)):
+            result = operators_variation[i](np.array([result, arguments[i]+1]), None)
+        return result
 
     # ----------------------- training -----------------------
 
