c12a5b57a0aeb66fa36ddff92fd9b9199e76831f
klima7 <ukasz.klimkiewicz@gmail.com>
2022-11-24 12:56:07 +0100

    Reformat ANFIS.py

--- ANFIS.py
+++ ANFIS.py
@@ -10,40 +10,41 @@ from optimizers import BaseOptimizer
 
 
 class ANFIS:
-    
-    def __init__(self, inputs, training_data: np.ndarray, expected_labels: np.ndarray, operator_function=productN, operator_init_value=0.5):
+
+    def __init__(self, inputs, training_data: np.ndarray, expected_labels: np.ndarray, operator_function=productN,
+                 operator_init_value=0.5):
         self.input_list = inputs
         self.input_number = len(inputs)
         self.training_data = training_data
         self.expected_labels = expected_labels
-        
+
         self.premises = []
         for i in range(self.input_number):
             self.premises.append(self.input_list[i].get())
 
         self.nodes_number = np.prod([inp.n_functions for inp in self.input_list])
-        
-        self.operator_function=operator_function
-        
-        #self.tsk = np.ones((self.nodes_number ,self.input_number+1))
-        self.tsk = np.random.random((self.nodes_number ,self.input_number+1))
-        
-        self.op  = [operator_init_value]*self.nodes_number
+
+        self.operator_function = operator_function
+
+        # self.tsk = np.ones((self.nodes_number ,self.input_number+1))
+        self.tsk = np.random.random((self.nodes_number, self.input_number + 1))
+
+        self.op = [operator_init_value] * self.nodes_number
 
         self.calculate_aids()
 
-    #Wyswietlanie funkcji przynależnosci
+    # Wyswietlanie funkcji przynależnosci
     def show_inputs(self):
         plt.figure()
         for i in range(self.input_number):
-            plt.subplot(self.input_number, 1, i+1)
+            plt.subplot(self.input_number, 1, i + 1)
             self.input_list[i].show()
             plt.legend()
         plt.show()
 
     def set_premises_parameters(self, fv):
         fv = np.array(fv).reshape(np.shape(self.premises))
-        self.premises=fv
+        self.premises = fv
         for i in range(self.input_number):
             self.input_list[i].set(*fv[i])
 
@@ -52,14 +53,14 @@ class ANFIS:
 
     def set_op_parameter(self, op):
         self.op = np.array(op).flatten()
-             
+
     def calculate_aids(self):
-        self.premises_combinations = np.array(calculate_combinations(self))[:,::-1]
+        self.premises_combinations = np.array(calculate_combinations(self))[:, ::-1]
 
     def output_to_labels(self, y_pred):
         rounded = np.round(y_pred.flatten()).astype(int)
         r_shape = np.shape(rounded)
-        return np.max((np.min((rounded , np.ones(r_shape)),axis=0), np.zeros(r_shape)),axis=0)#clamp 0-1       
+        return np.max((np.min((rounded, np.ones(r_shape)), axis=0), np.zeros(r_shape)), axis=0)  # clamp 0-1
 
     def estimate_labels(self):
         return self.anfis_estimate_labels(self.premises, self.op, self.tsk)
@@ -69,47 +70,47 @@ class ANFIS:
         data = self.training_data
 
         self.set_premises_parameters(fv)
-        tsk=np.reshape(tsk,np.shape(self.tsk))
+        tsk = np.reshape(tsk, np.shape(self.tsk))
         memberships = [self.input_list[x].fuzzify(data[x]) for x in range(self.input_number)]
-        
-        #Wnioskowanie
+
+        # Wnioskowanie
         arguments = []
         for premises in self.premises_combinations:
             item = []
             for i in range(len(premises)):
-                item.append(np.array(memberships[i])[:,premises[i]])
-            arguments.append(   item  )
+                item.append(np.array(memberships[i])[:, premises[i]])
+            arguments.append(item)
 
-        arguments = np.transpose(arguments, (1,2,0))
-        
-        R=self.operator_function(arguments, op)
-        
-        #Normalizacja normalizacja poziomów aktywacji reguł
+        arguments = np.transpose(arguments, (1, 2, 0))
+
+        R = self.operator_function(arguments, op)
+
+        # Normalizacja normalizacja poziomów aktywacji reguł
         Rsum = np.sum(R, axis=1, keepdims=True)
 
-        Rnorm = np.divide(R, Rsum, out=np.zeros_like(R), where=Rsum!=0)
-        Rnorm[(Rsum==0).flatten(),:] = 0
+        Rnorm = np.divide(R, Rsum, out=np.zeros_like(R), where=Rsum != 0)
+        Rnorm[(Rsum == 0).flatten(), :] = 0
         # wylicz wartoci przesłanek dla każdej próbki
-        
-        dataXYZ1 = np.vstack((self.training_data,np.ones(len(self.training_data[0])))).T
-        Q = np.dot(dataXYZ1, tsk.T) 
-        
+
+        dataXYZ1 = np.vstack((self.training_data, np.ones(len(self.training_data[0])))).T
+        Q = np.dot(dataXYZ1, tsk.T)
+
         # wyznacz wyniki wnioskowania dla każdej próbki
         result = (Q * Rnorm).sum(axis=1, keepdims=True)
-    
-        return result.T    
-        
+
+        return result.T
+
     def show_results(self, color=None):
         fig = plt.figure()
         ax = fig.add_subplot(111, projection='3d')
-        
-        if color is None:
-            color = [[1,0,0] if cc else [0,1,0] for cc in self.expected_labels]
 
-        result = self.anfis_estimate_labels(self.premises,self.op,self.tsk)
+        if color is None:
+            color = [[1, 0, 0] if cc else [0, 1, 0] for cc in self.expected_labels]
+
+        result = self.anfis_estimate_labels(self.premises, self.op, self.tsk)
         ax.scatter(self.training_data[0], self.training_data[1], result, c=color)
         plt.show()
-    
+
     def set_training_and_testing_data(self, training_data, expected_labels):
         self.training_data = training_data
         self.expected_labels = expected_labels
